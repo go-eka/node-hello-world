06da15c9174c490967c61ffba4f93878f7fca76c
Joseph Yu <joeyu629@gmail.com>
2022-01-19 21:15:34 -0800

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is up and listening on port ${process.env.PORT || 3000}`);
+});
